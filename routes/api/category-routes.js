7950a2dc8ecfbadae0c9e29868640d5861cb51d4
Zion Yang <zionremnant@gmail.com>
2022-04-05 04:04:46 -0700

    create new & update cat category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -33,15 +33,38 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
+  try {
+    const newCat = await Category.creat(req.body);
+    res.status(200).json(newCat);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  try {
+    const catData = await Category.update(
+      { category_name: req.body.category_name },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    const a = catData[0];
+    if (a === 0) {
+      res.status(404).json({ message: "Category not found" });
+    }
+    res.status(200).json({ message: "Category updated" });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
 });
 
