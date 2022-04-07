68ad3b933fd782480f540884730f8e5c5646dec4
Zion Yang <zionremnant@gmail.com>
2022-04-06 22:55:53 -0700

    fix minor errors

--- models/index.js
+++ models/index.js
@@ -6,20 +6,24 @@ const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
 Product.belongsTo(Category, { foreignKey: "category_id" });
+
 // Categories have many Products
 Category.hasMany(Product, { foreignKey: "category_id" });
+
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
   onDelete: "CASCADE",
   foreignKey: "product_id",
 });
+
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
   onDelete: "CASCADE",
   foreignKey: "tag_id",
 });
+
 module.exports = {
   Product,
   Category,
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -43,10 +43,10 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
   try {
-    const updateTag = await Tag.update(
+    const updatedTag = await Tag.update(
       { tag_name: req.body.tag_name },
       {
         where: {
@@ -60,7 +60,7 @@ router.put("/:id", (req, res) => {
   }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete on tag by its `id` value
   try {
     const deleteTag = await Tag.destroy({ where: { id: req.params.id } });
